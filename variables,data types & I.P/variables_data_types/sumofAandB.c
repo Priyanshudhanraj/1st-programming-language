7605df08ce725d23be17ebfc158efd03ea73e520
Priyanshudhanraj <priyanshudhanraj2007@gmail.com>
2026-07-28 20:23:57 +0530

    Add Variables_data_types folder and sum of A and B program

---
+++ variables,data types & I.P/variables_data_types/sumofAandB.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int a, b;
+    printf ("enter A");
+    scanf ("%d", &a);
+    printf ("enter B");
+    scanf ("%d", &b);
+    int sum = a + b;
+    printf ("sum of A and B is %d", sum);
+    return 0;
+}
