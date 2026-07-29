f265475f746d9a4809ba7fc1f476a900bee7248f
Priyanshudhanraj <priyanshudhanraj2007@gmail.com>
2026-07-29 15:28:16 +0530

    Cube of a Number

---
+++ variables,data types & I.P/variables_data_types/cube_of_a_number.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main ()
+{
+    
+ int number;
+ printf("Enter a number: ");
+    scanf("%d", &number);
+    printf("Cube is: %d", number * number * number);
+    return 0;
+
+}
