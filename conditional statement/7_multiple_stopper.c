27952ec7f1b303f3c0c9d909c2da19b1ee31cbf5
Priyanshudhanraj <priyanshudhanraj2007@gmail.com>
2026-08-14 21:07:58 +0530

    7 multiple stopper

---
+++ conditional statement/7_multiple_stopper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    do {
+        printf("Enter a number: ");
+        scanf("%d", &n);
+        printf("%d\n", n);
+
+        if (n % 7 == 0) {   // multiple of 7
+            break;
+        }
+
+    } while (1);
+
+    printf("Thank you!");
+
+    return 0;
+}
