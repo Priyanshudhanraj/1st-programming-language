3c6744be86d001b35b9f5d261d4260e950d5be8f
Priyanshudhanraj <priyanshudhanraj2007@gmail.com>
2026-08-22 11:02:13 +0530

    Smallest of two number

---
+++ conditional statement/smallest_of_2_number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    if (a < b) {
+        printf("Smallest number is %d\n", a);
+    } else {
+        printf("Smallest number is %d\n", b);
+    }
+
+    return 0;
+}
